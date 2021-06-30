cee2dca16ab8e9746dd91668f932745cf8862d10
yjein <yjein4220@raonfriends.com>
2021-06-30 17:48:47 +0900

    [추가] blog 모듈

---
+++ nest-project/src/blog/blog.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Blog } from './entities/blog.entity';
+import { BlogController } from './blog.controller';
+import { BlogService } from './blog.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Blog])],
+  providers: [BlogService],
+  controllers: [BlogController],
+})
+export class BlogModule {}
