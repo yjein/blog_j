fc1c7b21c55f172ee4d3a4fadbb7e05d5aa476aa
yjein <yjein4220@raonfriends.com>
2021-07-05 07:08:52 +0900

    [수정] app fetch

--- react-project/src/Pages/App.tsx
+++ react-project/src/Pages/App.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { findAll, create } from "../api/blog.api";
 
-function App() {
-  return <div></div>;
+interface Blog {
+  id: string;
+  title: String;
+  contents: string;
 }
+
+const App = () => {
+  const [datas, setDatas] = useState<Blog[]>([]);
+  // const [loading, setLoading] = useState<Boolean>(true);
+
+  const post = async (data: Blog) => {
+    const blogData = {
+      title: data.title,
+      contents: data.contents,
+    };
+
+    create(blogData);
+  };
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      const resp = await findAll();
+
+      setDatas(resp);
+    };
+
+    fetchAll();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog</h1>
+      <form
+        onSubmit={(e) => {
+          // post();
+        }}
+      >
+        <input type="text" required={true} size={54} placeholder="Title" />
+        <textarea required={true} cols={50} rows={30}></textarea>
+        <button>Post</button>
+      </form>
+      {datas.map((data, idx) => {
+        return (
+          <li key={idx}>
+            {data.contents}
+            <div>{data.contents}</div>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
