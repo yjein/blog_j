4b6b6a938a46254b2db2d1822ca197dc9a664f26
yjein <yjein4220@raonfriends.com>
2021-07-06 04:17:32 +0900

    [수정] Create 구동 되도록 수정

--- react-project/src/Organisms/Create.tsx
+++ react-project/src/Organisms/Create.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-// import { Blog, BlogPageType } from "../Pages/App";
+import { Blog, BlogPageType } from "../Pages/App";
 
-const Form = styled.div`
+const Form = styled.form`
   display: flex;
   flex-flow: column;
   margin: 1rem 0;
@@ -10,27 +10,49 @@ const Form = styled.div`
 `;
 
 interface Props {
-  // data: Blog;
-  // setPageStatus: React.Dispatch<React.SetStateAction<BlogPageType>>;
+  setPageStatus: React.Dispatch<React.SetStateAction<BlogPageType>>;
+  createBlog: (data: Blog) => Promise<void>;
 }
 
 const Create: React.FC<Props> = (props) => {
-  // const { data, setPageStatus } = props;
+  const { setPageStatus, createBlog } = props;
+  const [value, setValue] = useState<Blog>({ title: "", contents: "" });
 
   return (
-    <Form
-      onSubmit={(e) => {
-        // post();
-      }}
-    >
-      <input type="text" required={true} size={54} placeholder="Title" />
-      <textarea
-        required={true}
-        cols={50}
-        rows={30}
-        placeholder="Contents"
-      ></textarea>
-    </Form>
+    <>
+      <button onClick={() => setPageStatus("main")}>Back</button>
+
+      <Form
+        onSubmit={(e) => {
+          console.log("submt????????");
+          e.preventDefault();
+
+          createBlog(value);
+        }}
+      >
+        <button type="submit" onClick={() => console.log("submit")}>
+          Submit
+        </button>
+
+        <input
+          type="text"
+          required={true}
+          placeholder="Title"
+          onChange={(e) => {
+            setValue({ title: e.target.value, contents: value.contents });
+          }}
+        />
+        <textarea
+          required={true}
+          cols={50}
+          rows={30}
+          placeholder="Contents"
+          onChange={(e) => {
+            setValue({ title: value.title, contents: e.target.value });
+          }}
+        />
+      </Form>
+    </>
   );
 };
 
