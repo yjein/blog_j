08c32144ba34b7426cb3749b71f484560aa442ae
yjein <yjein4220@raonfriends.com>
2021-07-05 17:52:52 +0900

    [추가] tesk 컴포넌트

---
+++ react-project/src/Organisms/Task.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { Blog, BlogPageType } from "../Pages/App";
+
+const Wrap = styled.div`
+  margin: 1rem 0;
+  padding: 1rem;
+  border: 1px solid none;
+  border-radius: 0.5rem;
+  box-shadow: 1px 1px 6px 1px #c0bfbf;
+  cursor: pointer;
+`;
+
+const H2 = styled.h2`
+  margin: 0;
+  padding: 0;
+`;
+
+const Div = styled.div`
+  margin-top: 0.5rem;
+  list-style: none;
+`;
+
+interface Props {
+  tasks: Blog[];
+  setPageStatus: React.Dispatch<React.SetStateAction<BlogPageType>>;
+}
+
+const Task: React.FC<Props> = (props) => {
+  const { tasks, setPageStatus } = props;
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setPageStatus("create");
+        }}
+      >
+        Post
+      </button>
+      {tasks.map((data) => {
+        return (
+          <Wrap
+            onClick={() => {
+              setPageStatus("read");
+            }}
+          >
+            <H2>{data.contents}</H2>
+            <Div>{data.contents}</Div>
+          </Wrap>
+        );
+      })}
+    </>
+  );
+};
+
+export default Task;
