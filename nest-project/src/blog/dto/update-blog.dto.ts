535fc5875b4c91892d016239730187e682719f7c
yjein <yjein4220@raonfriends.com>
2021-07-07 18:13:25 +0900

    [수정] 업데이트 DTO

--- nest-project/src/blog/dto/update-blog.dto.ts
+++ nest-project/src/blog/dto/update-blog.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { ObjectID } from 'typeorm';
 import { CreateBlogDto } from './create-blog.dto';
 
-export class UpdateBlogDto extends PartialType(CreateBlogDto) {
-  _id: ObjectID;
-}
+export class UpdateBlogDto extends PartialType(CreateBlogDto) {}
