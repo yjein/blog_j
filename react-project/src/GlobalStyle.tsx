556ebd9350d2684d45d98c9ad0f996edbf334acc
yjein <yjein4220@raonfriends.com>
2021-07-08 16:45:48 +0900

    [추가] 전역 스타일링 적용

---
+++ react-project/src/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html, body {
+  margin: 0;
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+}
+`;
+
+export default GlobalStyle;
--- react-project/src/index.tsx
+++ react-project/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Pages/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyle from "./GlobalStyle";
+import App from "./Pages/App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
