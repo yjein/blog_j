78edb56506371344364c2be465103933bd2d37b1
yjein <yjein4220@raonfriends.com>
2021-07-09 14:58:29 +0900

    [수정] 불필요하게 나눈 작업들 수정

--- nest-project/src/blog/blog.controller.ts
+++ nest-project/src/blog/blog.controller.ts
@@ -6,7 +6,6 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
@@ -19,36 +18,26 @@ export class BlogController {
 
   @Get()
   findAll(): Promise<Blog[]> {
-    const blog = this.blogService.findAll();
-
-    return blog;
+    return this.blogService.findAll();
   }
 
   @Get(':id')
   async findOne(@Param('id') _id: string): Promise<Blog> {
-    const blog = this.blogService.findOne(_id);
-
-    return blog;
+    return this.blogService.findOne(_id);
   }
 
   @Post()
   create(@Body() createBlogDto: CreateBlogDto) {
-    const blog = this.blogService.create(createBlogDto);
-
-    return blog;
+    return this.blogService.create(createBlogDto);
   }
 
   @Delete(':id')
   async remove(@Param('id') _id: string) {
-    const blog = await this.blogService.deleteOne(_id);
-
-    return blog;
+    return await this.blogService.deleteOne(_id);
   }
 
   @Patch(':id')
   async patch(@Param('id') _id: string, @Body() updateBlogDto: UpdateBlogDto) {
-    const blog = await this.blogService.update(_id, updateBlogDto);
-
-    return blog;
+    return await this.blogService.update(_id, updateBlogDto);
   }
 }
